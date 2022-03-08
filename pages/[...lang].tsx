87b91e9c9f595730ca96bd73bb8629d903600c1e
Zachary Milonas <zachary@milonas.pl>
2022-03-08 17:37:19 +0100

    Remove pages

--- pages/[...lang].tsx
+++
@@ -1,9 +0,0 @@
-import {useRouter} from 'next/router'
-import Index from "./Index";
-
-export default () => {
-    const router = useRouter()
-    const {lang} = router.query
-
-    return <Index lang={lang} />
-}
