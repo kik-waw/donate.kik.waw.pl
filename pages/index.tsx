2c61418cb3c081dac43180a13aff0b52794246b3
Zachary Milonas <zachary@milonas.pl>
2022-03-08 23:43:33 +0100

    Render default EN instead of redirecting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import {useRouter} from "next/router";
+import {type NextPage} from "next";
 import React from "react";
 import getNavigatorLanguage from "../components/getNavigatorLanguage";
+import LocalePage, {getStaticProps as localeGetStaticProps} from './[locale]';
+import {PageProps} from "../components/Page";
 
-const Page = () => {
+const Page: NextPage<PageProps> = (props) => {
     const router = useRouter();
 
     React.useEffect(() => {
@@ -10,9 +13,11 @@ const Page = () => {
         router.push(`/${language}`);
     });
 
-    return <>
-        <meta httpEquiv="refresh" content="1; url=/en" />
-    </>
+    return <LocalePage {...props} />
+}
+
+export async function getStaticProps() {
+    return localeGetStaticProps({ params: { locale: 'en' }})
 }
 
 export default Page;
