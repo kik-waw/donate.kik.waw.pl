24989d4d18ad9593758010df1a23dce86e218857
Dominik Gubrynowicz <dominik.gubrynowicz@gmail.com>
2022-03-11 21:22:19 +0100

    Changed header style to match the new design

---
+++ components/LanguageSelector.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/router";
+
+const LanguageLink: React.FC = ({ children: localeSource }) => {
+    const { query } = useRouter();
+    const locale = localeSource.toLowerCase();
+    const isActive = query.locale === locale
+    return (
+        <div className={isActive ? 'language-select active' : 'language-select'}>
+            <Link href={`/${locale}`} passHref> 
+                <a>{localeSource}</a>
+            </Link>
+        </div>
+    );
+};
+
+const Languages: React.FC = () => <div className="text-3xl h-full w-1/2 lg:w-full text-right">
+    <div className="flex">
+        <LanguageLink>EN</LanguageLink>
+        <LanguageLink>PL</LanguageLink>
+        <LanguageLink>DE</LanguageLink>
+        <div className="kik-page-link">
+            <Link href={`https://www.kik.waw.pl/?no_redir=1`} passHref> 
+                <a>kik.waw.pl</a>
+            </Link>
+        </div>
+    </div>
+</div>
+
+
+export default Languages;
