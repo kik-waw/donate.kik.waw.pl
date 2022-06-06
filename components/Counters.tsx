33f310a0fab8ff1d690f62145c64e4a164f849c0
Dominik Gubrynowicz <56269092+Dominik-Gubrynowicz@users.noreply.github.com>
2022-06-06 18:21:08 +0200

    Updated counters (#7)

--- components/Counters.tsx
+++ components/Counters.tsx
@@ -9,12 +9,13 @@ const Counters: React.FC = () => {
             <section className="container text-white about">
                 <div>
                     <h2 className="text-grayblue pb-4">{t('What we did')}</h2>
-                    <Counter number={600}>{t('We evacuated')}</Counter>
-                    <Counter number={400}>{t('We hosted')}</Counter>
-                    <Counter number={140}>{t('We created base')}</Counter>
-                    <Counter number={10}>{t('We sent')}</Counter>
-                    <Counter number={4}>{t('We conduct afternoon club acivities')}</Counter>
-                    <Counter number={400}>{t('Volunteers')}</Counter>
+                    <Counter number={750}>{t('We evacuated')}</Counter>
+                    <Counter number={700}>{t('We hosted')}</Counter>
+                    <Counter number={170}>{t('We created base')}</Counter>
+                    <Counter number={12}>{t('We sent')}</Counter>
+                    <Counter number={300}>{t('We conduct afternoon club acivities')}</Counter>
+                    <Counter number={700}>{t('Volunteers')}</Counter>
+                    <Counter number={300}>{t('School')}</Counter>
                 </div>
             </section>
         </div>
