86f3f68b84969e8e0758fa50785118fcbd53bf10
Dominik Gubrynowicz <dominik.gubrynowicz@gmail.com>
2022-11-30 16:45:02 +0100

    feat: add spacing

--- components/BankTransfer.tsx
+++ components/BankTransfer.tsx
@@ -7,16 +7,16 @@ const BankTransfer: React.FC = () => {
     return (
         <div className="w-full bg-grayblue">
             <section className="container about text-white">
-                <div>
+                <div className="w-full">
                     <h2 className="mb-12">{t('Support us via a wire transfer!')}</h2>
-                    <ul className="">
+                    <ul className="w-full">
                         <li>
-                            {t('Klub Inteligencji Katolickiej')}<br/>
-                            <b>{t('Transfer title')}:</b> darowizna na cele statutowe - ukraina <br/>
-                            <b>SWIFT:</b> BREXPLPWXXX <br/>
-                            <b>PLN IBAN:</b> PL69 1140 2062 0000 4445 3900 1005 <br/>
-                            <b>EUR IBAN:</b> PL47 1140 2062 0000 4445 3900 1013 <br/>
-                            <b>USD IBAN:</b> PL90 1140 2062 0000 4445 3900 1015 <br/>
+                            <b>{t('Klub Inteligencji Katolickiej')}</b><br/>
+                            <b>{t('Transfer title')}:</b><br/> Darowizna na cele statutowe - ukraina <br/>
+                            <b>SWIFT:</b> <br/>BREXPLPWXXX <br/>
+                            <b>PLN IBAN:</b> <br/>PL69 1140 2062 0000 4445 3900 1005 <br/>
+                            <b>EUR IBAN:</b> <br/>PL47 1140 2062 0000 4445 3900 1013 <br/>
+                            <b>USD IBAN:</b> <br/>PL90 1140 2062 0000 4445 3900 1015 <br/>
                         </li>
                     </ul>
                     <div className="w-4/5 md:w-1/2 mt-6">
